bbd765d8d236f602d704fe9fa0ec630bf53b91be
allyourbrowserarebelongtous <M8R-fwg47k@mailinator.com>
2012-06-27 02:27:28 -0400

    path fix to md5_crack example

--- example/md5_crack.js
+++ example/md5_crack.js
@@ -3,7 +3,7 @@ function(bshell) {
 	return false;
     }
 
-    bshell.plugins.md5.loadDictionary('plugins/md5/dict.txt');
-    bshell.plugins.md5.testHash('5d41402abc4b2a76b9719d911017c592');
+    bshell.plugins.md5.loadDictionary(bshell.root_url + 'plugins/md5/dict.txt');
+    bshell.plugins.md5.testHash('5d41402abc4b2a76b9719d911017c592');  // hello
     return false;
 }(bshell);
